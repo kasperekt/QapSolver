7ee789ec1f11ed2d876908f3c1c7ee21e6cba812
Tomasz Kasperek <kasperekt@gmail.com>
2018-10-27 13:17:59 +0200

    Improve parsing

---
+++ QapSolver/Solvers/QapLocalSolverGreedy.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using QapSolver;
+using ArrayExtensions;
+
+namespace QapSolver.Solvers
+{
+  class QapLocalSolverGreedy : QapProblemSolver
+  {
+    public QapLocalSolverGreedy(QapProblemInstance instance) : base(instance) { }
+
+    public override QapProblemSolution Solve()
+    {
+      var assignments = GetRandomAssignments(Instance.Size);
+      var cost = GetCost(assignments);
+      bool progress = true;
+      int iterationCounter = 0;
+
+      while (progress)
+      {
+        progress = false;
+        iterationCounter++;
+
+        var neighbours = GetNeighbours(assignments);
+
+        foreach (var neighbour in neighbours)
+        {
+          var neighbourCost = GetCost(neighbour);
+
+          if (neighbourCost < cost)
+          {
+            assignments = neighbour;
+            cost = neighbourCost;
+            progress = true;
+            break;
+          }
+        }
+      }
+
+      return new QapProblemSolution(assignments, cost);
+    }
+
+    private List<int[]> GetNeighbours(int[] assignments)
+    {
+      List<int[]> neighbours = new List<int[]>();
+
+      for (int i = 0; i < assignments.Length; i++)
+      {
+        for (int j = i + 1; j < assignments.Length; j++)
+        {
+          var neighbour = (assignments.Clone() as int[]);
+          neighbour.Swap(i, j);
+
+          neighbours.Add(neighbour);
+        }
+      }
+
+      return neighbours;
+    }
+  }
+}
