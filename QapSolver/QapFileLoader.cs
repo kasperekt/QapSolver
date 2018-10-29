28f005ff1b0b81c3da50453968b348a611e93a62
Tomasz Kasperek <kasperekt@gmail.com>
2018-10-29 16:53:19 +0100

    Proper loader (with data fixed)

--- QapSolver/QapFileLoader.cs
+++ QapSolver/QapFileLoader.cs
@@ -14,60 +14,53 @@ namespace QapSolver
       BasePath = basePath;
     }
 
+    private int[] GetRowValues(ref StreamReader sr, int size)
+    {
+      var allLines = new List<int>();
+
+      while (allLines.Count < size)
+      {
+        var lines = sr.ReadLine().Split(new char[] { }, StringSplitOptions.RemoveEmptyEntries);
+        foreach (var element in lines)
+        {
+          allLines.Add(Int32.Parse(element));
+        }
+      }
+
+      return allLines.ToArray();
+    }
+
     public QapProblemInstance Load(string name)
     {
       string filePath = $"{BasePath}/{name}";
+      StreamReader sr = new StreamReader(filePath);
 
-      using (StreamReader sr = new StreamReader(filePath))
+      int size = Int32.Parse(sr.ReadLine());
+      var flows = new int[size, size];
+      var distances = new int[size, size];
+
+      for (int i = 0; i < size; i++)
       {
-        String line = sr.ReadLine();
-
-        int size = Int32.Parse(line);
-        sr.ReadLine(); // First empty line
-
-        var flows = new int[size, size];
-        var distances = new int[size, size];
-
-        for (int i = 0; i < size; i++)
+        var allLines = GetRowValues(ref sr, size);
+        for (int j = 0; j < size; j++)
         {
-          var allLines = new List<int>();
-          while (allLines.Count < size)
-          {
-            var lines = sr.ReadLine().Split(new char[] { }, StringSplitOptions.RemoveEmptyEntries);
-            foreach (var element in lines)
-            {
-              allLines.Add(Int32.Parse(element));
-            }
-          }
-
-          for (int j = 0; j < size; j++)
-          {
-            flows[i, j] = allLines[j];
-          }
+          flows[i, j] = allLines[j];
         }
-
-        sr.ReadLine(); // Second empty line
-
-        for (int i = 0; i < size; i++)
-        {
-          var allLines = new List<int>();
-          while (allLines.Count < size)
-          {
-            var lines = sr.ReadLine().Split(new char[] { }, StringSplitOptions.RemoveEmptyEntries);
-            foreach (var element in lines)
-            {
-              allLines.Add(Int32.Parse(element));
-            }
-          }
-
-          for (int j = 0; j < size; j++)
-          {
-            distances[i, j] = allLines[j];
-          }
-        }
-
-        return new QapProblemInstance(size, flows, distances);
       }
+
+      for (int i = 0; i < size; i++)
+      {
+        var allLines = GetRowValues(ref sr, size);
+        for (int j = 0; j < size; j++)
+        {
+          distances[i, j] = allLines[j];
+        }
+      }
+
+      sr.Close();
+
+      return new QapProblemInstance(size, flows, distances);
+
     }
   }
 }
