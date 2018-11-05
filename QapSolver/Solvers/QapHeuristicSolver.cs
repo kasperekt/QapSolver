765eb00a84c3b2f51eb1149eb0d008d1a77f2ebf
Tomasz Kasperek <kasperekt@gmail.com>
2018-11-05 18:36:29 +0100

    Heuristic algorithm

--- QapSolver/Solvers/QapHeuristicSolver.cs
+++ QapSolver/Solvers/QapHeuristicSolver.cs
@@ -1,5 +1,8 @@
 using System;
 using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Linq;
+using ArrayExtensions;
 
 namespace QapSolver.Solvers
 {
@@ -18,11 +21,85 @@ namespace QapSolver.Solvers
 
       // TODO: Here!!!
       var assignments = GetRandomAssignments(Instance.Size);
-      var cost = GetCost(assignments);
+      bool progress = true;
+      int iterationCounter = 0;
+
+      while (progress)
+      {
+        progress = false;
+        iterationCounter++;
+
+        var worstIdx = GetWorstIndex(assignments);
+        var bestReplacement = GetBestReplacement(worstIdx, assignments);
+
+        if (bestReplacement != worstIdx)
+        {
+          assignments.Swap(worstIdx, bestReplacement);
+          progress = true;
+        }
+      }
 
       watch.Stop();
 
-      return new QapProblemSolution(assignments, cost, 0, 0, watch.ElapsedMilliseconds);
+      return new QapProblemSolution(
+        solution: assignments,
+        cost: GetCost(assignments),
+        steps: iterationCounter,
+        visited: iterationCounter,
+        timeMs: watch.ElapsedMilliseconds
+      );
+    }
+
+    private int GetBestReplacement(int i, int[] assignments)
+    {
+      var bestReplacement = i;
+      var bestReplacementCost = int.MaxValue;
+
+      for (int j = 0; j < Instance.Size; j++)
+      {
+        var nextAssignments = (assignments.Clone() as int[]);
+        nextAssignments.Swap(i, j);
+        var cost = GetCost(nextAssignments);
+
+        if (cost < bestReplacementCost)
+        {
+          bestReplacement = j;
+          bestReplacementCost = cost;
+        }
+      }
+
+      return bestReplacement;
+    }
+
+    private int GetWorstIndex(int[] assignments)
+    {
+      int size = Instance.Size;
+
+      int worstIdx = 0;
+      int worstValue = int.MinValue;
+
+      for (int i = 0; i < size; i++)
+      {
+        var iValue = 0;
+
+        for (int j = 0; j < size; j++)
+        {
+          if (i == j)
+          {
+            continue;
+          }
+
+          iValue += Instance.Flows[i, j] * Instance.Distances[assignments[i], assignments[j]];
+        }
+
+        if (iValue > worstValue)
+        {
+          worstIdx = i;
+          worstValue = iValue;
+        }
+      }
+
+      return worstIdx;
     }
   }
 }
