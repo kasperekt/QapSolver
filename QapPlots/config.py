ae94543fe84010efbc809514b8194ee8b2c07ea3
Tomasz Kasperek <kasperekt@gmail.com>
2018-11-12 14:52:32 +0100

    Add plots

--- QapPlots/config.py
+++ QapPlots/config.py
@@ -1,3 +1,6 @@
-solvers = ['random-solver', 'heuristic-solver', 'local-greedy-solver', 'local-steepest-solver']
-rounds = [100, 150, 200, 250, 300]
-instances = ['chr18a', 'chr20a', 'tai12a', 'tai12b', 'tai15a', 'tai35b', 'esc32g', 'lipa50a']
+solvers = ['random-solver', 'heuristic-solver',
+           'local-greedy-solver', 'local-steepest-solver']
+# rounds = [100, 150, 200, 250, 300]
+rounds = [300]
+instances = ['chr18a', 'chr20a', 'tai12a', 'tai12b',
+             'tai15a', 'tai35b', 'esc32g', 'lipa50a']
--- QapPlots/helpers.py
+++ QapPlots/helpers.py
@@ -1,3 +1,10 @@
 def quality(solution, optimal):
     '''Optimal is always less or equal.'''
     return optimal / solution
+
+
+def effectiveness(quality, time):
+    if time == 0:
+        return 0
+
+    return quality / time
