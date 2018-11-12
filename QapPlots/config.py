71537e7c3ab49328253dcc9f1f2037858201074e
Tomasz Kasperek <kasperekt@gmail.com>
2018-11-12 11:17:02 +0100

    Change legend position for multiple restarts

---
+++ QapPlots/config.py
@@ -0,0 +1,3 @@
+solvers = ['random-solver', 'heuristic-solver', 'local-greedy-solver', 'local-steepest-solver']
+rounds = [100, 150, 200, 250, 300]
+instances = ['chr18a', 'chr20a', 'tai12a', 'tai12b', 'tai15a', 'tai35b', 'esc32g', 'lipa50a']
--- QapPlots/multiple_restarts.py
+++ QapPlots/multiple_restarts.py
@@ -2,50 +2,55 @@ import matplotlib.pyplot as plt
 import numpy as np
 import re
 
-def get_optimum(file):
-  with open(file, 'r') as fh:
-    data = fh.read()
 
-  return parse_optimal_solution(data)[0]
+def get_optimum(file):
+    with open(file, 'r') as fh:
+        data = fh.read()
+
+    return parse_optimal_solution(data)[0]
+
 
 def parse_optimal_solution(data):
-  data = re.sub(' +', ' ', data).split('\n')
+    data = re.sub(' +', ' ', data).split('\n')
 
-  optimal_result = int(data[0].split(' ')[2])
-  optimal_perm = data[1].split(' ')[1:-1]
+    optimal_result = int(data[0].split(' ')[2])
+    optimal_perm = data[1].split(' ')[1:-1]
 
-  return optimal_result, optimal_perm
+    return optimal_result, optimal_perm
+
 
 def quality(solution, optimal):
-  return 1 - ((solution - optimal) / solution)
+    return 1 - ((solution - optimal) / solution)
+
 
 def get_results(file):
-  with open(file, 'r') as fh:
-    data = fh.read()
+    with open(file, 'r') as fh:
+        data = fh.read()
 
-  best = []
-  avg = []
-  scores = []
+    best = []
+    avg = []
+    scores = []
 
-  for i, line in enumerate(data.split('\n')):
-    if i==0 or i == (size+1): continue
-    parsed = line.split(',')
+    for i, line in enumerate(data.split('\n')):
+        if i == 0 or i == (size+1):
+            continue
+        parsed = line.split(',')
 
-    score = int(parsed[1])
-    if i==1:
-        best.append(score)
+        score = int(parsed[1])
+        if i == 1:
+            best.append(score)
+            scores.append(score)
+            avg.append(score)
+            continue
+
+        if score < best[i-2]:
+            best.append(score)
+        else:
+            best.append(best[i-2])
+
         scores.append(score)
-        avg.append(score)
-        continue
-
-    if score < best[i-2]:
-        best.append(score)
-    else:
-        best.append(best[i-2])
-
-    scores.append(score)
-    avg.append(np.mean(scores))
-  return best, avg
+        avg.append(np.mean(scores))
+    return best, avg
 
 
 algs = {
@@ -60,34 +65,36 @@ algs = {
     }
 }
 
-all_problems = ['chr18a', 'chr20a', 'esc32g', 'lipa50a', 'tai12a', 'tai12b', 'tai15a', 'tai35b']
+all_problems = ['chr18a', 'chr20a', 'esc32g',
+                'lipa50a', 'tai12a', 'tai12b', 'tai15a', 'tai35b']
 selected_problems = ['tai12a', 'tai35b']
 size = 300
-plt.figure(figsize=(15,10))
+plt.figure(figsize=(15, 10))
 
 x = np.arange(1, 301, 1)
 
 for problem in selected_problems:
-  optimal_score = get_optimum('../QapData/' + problem + '.sln')
+    optimal_score = get_optimum('../QapData/' + problem + '.sln')
 
-  alg_results = {}
-  for alg in algs.keys():
-      alg_results[alg] = {}
-      alg_data_best, alg_data_avg = get_results('../QapSolver/results/' + problem + '_' + alg + '_' + str(size) + '.csv')
-      alg_results[alg]['best'] = list(map(lambda x: quality(x, optimal_score), alg_data_best))
-      alg_results[alg]['avg'] = list(map(lambda y: quality(y, optimal_score), alg_data_avg))
+    alg_results = {}
+    for alg in algs.keys():
+        alg_results[alg] = {}
+        alg_data_best, alg_data_avg = get_results(
+            '../QapSolver/results/' + problem + '_' + alg + '_' + str(size) + '.csv')
+        alg_results[alg]['best'] = list(
+            map(lambda x: quality(x, optimal_score), alg_data_best))
+        alg_results[alg]['avg'] = list(
+            map(lambda y: quality(y, optimal_score), alg_data_avg))
 
+    for alg in algs.keys():
+        plt.plot(x, alg_results[alg]['best'],
+                 label=algs[alg]['label'] + "_" + problem + "_" + 'best', alpha=0.8, linestyle=algs[alg]['linestyle'])
 
-  for alg in algs.keys():
-      plt.plot(x, alg_results[alg]['best'],
-                  label=algs[alg]['label'] + "_" + problem + "_" + 'best', alpha=0.8, linestyle=algs[alg]['linestyle'])
-
-  for alg in algs.keys():
-      plt.plot(x, alg_results[alg]['avg'],
-                  label=algs[alg]['label'] + "_" + problem + "_" + 'avg', alpha=0.8, linestyle=algs[alg]['linestyle'])
+    for alg in algs.keys():
+        plt.plot(x, alg_results[alg]['avg'],
+                 label=algs[alg]['label'] + "_" + problem + "_" + 'avg', alpha=0.8, linestyle=algs[alg]['linestyle'])
 
 plt.xlabel('liczba iteracji')
 plt.ylabel('jakość rozwiązania')
-plt.legend(bbox_to_anchor=(1.05, 1))
+plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), mode='expand', ncol=4, loc=3)
 plt.savefig('multiple_restarts.png')
-
